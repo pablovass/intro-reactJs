4ea2d573bf28ddd0c8d761f2ff246e7938df105e
pablo vallejos <pablo.vallejos@naturalth.it>
2022-12-14 16:42:23 -0300

    creando mi app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import './App.css';
+const todos = [
+  { text: "Cortar cebolla", completed: false },
+  { text: "Tomar terminar la tarea", completed: false },
+  { text: "Llorar con la llorona", completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende con pablo
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {/*<TodoCounter/>*/}
+      <h2>Has completado 2 de 3 TODOs</h2>
+      {/*<TodoSearch />*/}
+      <input placeholder="Cebolla" />
+
+      {/*<TodoList>*/}
+      {todos.map((todo) => ({
+        /*<TodoItem />*/
+      }))}
+
+      {/* <TodoList/>*/}
+      {/* <CreateTodoButton/>*/}
+      <button>**</button>
+    </React.Fragment>
   );
 }
 
