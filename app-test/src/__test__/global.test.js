0fd9784e8b5bfc4aba9f046b7fffcbd76bc2a2fc
pablo vallejos <pablo.vallejos@naturalth.it>
2022-12-23 10:58:05 -0300

    1ras pruevas globales

--- app-test/src/__test__/global.test.js
+++ app-test/src/__test__/global.test.js
@@ -1,5 +1,23 @@
 const text = "Hola Mundo";
+const fruit = ["manzana", "melon", "banana"];
 
+test("deve contener una fruta", () => {
+  expect(fruit).toContain("manzana");
+});
 test("should have a text", () => {
   expect(text).toMatch(/Mundo/);
+});
+
+test("mayor que ", () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+const reverseString = (str, callback) => {
+  callback(str.split("").reverse().join(""));
+};
+
+test("probar un callback", () => {
+  reverseString("Hola", (str) => {
+    expect(str).toBe("aloH");
+  });
 });
