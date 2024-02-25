5d65ac16d1741e31f73e18f42d706fe7c802d07d
Pablo Vallejos <faktory.fm@live.com>
2024-02-25 17:22:36 -0300

    feat: tiene el boton de editar las tareas

--- app-react-birthday/src/components/ListTasks.jsx
+++ app-react-birthday/src/components/ListTasks.jsx
@@ -73,7 +73,7 @@ const Section = ({ status, tasks, setTasks, todos, inProgress, closed }) => {
         return t;
       });
       localStorage.setItem("tasks", JSON.stringify(mTasks));
-      toast("Task status change",{icon:"👌"})
+      toast("Task status change", { icon: "👌" });
       return mTasks;
       // console.log("prev",prev);
     });
@@ -147,6 +147,23 @@ const Task = ({ task, tasks, setTasks }) => {
             strokeLinecap="round"
             strokeLinejoin="round"
             d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+      </button>
+      <button className="absolute top-0 right-0 flex space-x-2"
+       onClick={() => handleEdit(task.id)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
           />
         </svg>
       </button>
