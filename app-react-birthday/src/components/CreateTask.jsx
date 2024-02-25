80b56464baac2a98ffea84b535cd8b20bd7872ad
Pablo Vallejos <faktory.fm@live.com>
2024-02-25 17:02:53 -0300

    ya tengo un formulario que puedo completarlo

--- app-react-birthday/src/components/CreateTask.jsx
+++ app-react-birthday/src/components/CreateTask.jsx
@@ -1,61 +1,25 @@
-import { useState, useEffect } from "react";
-import { toast } from "react-hot-toast";
-import { v4 as uuidv4 } from "uuid";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import ModalForm from "./ModalForm";
 
-const CreateTask = ({ tasks, setTasks }) => {
+const CreateTask = () => {
   const [showModalForm, setShowModalForm] = useState(false);
-  const [task, setTask] = useState({
-    id: "",
-    name: "",
-    status: "todo", //can also bi inprogress or closed
-  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (task.name.length < 3)
-      return toast.error("a task must have more than 3 characters");
-
-    if (task.name.length > 100)
-      return toast.error("A task must not be more than 100 characters");
-
-    setTasks((prev) => {
-      const list = prev ? [...prev, task] : [task]; // Use conditional spread
-      localStorage.setItem("tasks", JSON.stringify(list));
-      return list;
-    });
-    toast.success("Task Created");
-    setTask({
-      id: "",
-      name: "",
-      status: "todo",
-    });
+  const handleOutsideClick = (e) => {
+    if (showModalForm && !e.target.closest(".modal")) {
+      setShowModalForm(false);
+    }
   };
 
-  useEffect(() => {
-    const handleOutsideClick = (e) => {
-      if (showModalForm && !e.target.closest(".modal")) {
-        setShowModalForm(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleOutsideClick);
-    return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
-    };
-  }, [showModalForm]);
-
   return (
-    <form onSubmit={handleSubmit}>
-      <ModalForm onClose={() => setShowModalForm(false)} visible={showModalForm} />
+    <div onClick={handleOutsideClick}>
       <button
         className="bg-cyan-500 rounded-md px-4 h-12 text-white hover:scale-95 transition text-xl"
         onClick={() => setShowModalForm(true)}
       >
         CARGA PERSONA
       </button>
-    </form>
+      <ModalForm onClose={() => setShowModalForm(false)} visible={showModalForm} />
+    </div>
   );
 };
 
--- app-react-birthday/src/components/ModalForm.jsx
+++ app-react-birthday/src/components/ModalForm.jsx
@@ -1,18 +1,109 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const ModalForm = ({ visible, onClose }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    lastName: "",
+    birthdate: "",
+    documentNumber: "",
+    email: "",
+    phone: "",
+    statusDB: ""
+  });
+
+  const modalRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      onClose();
+    }
+  };
+
+  const handleFormClick = (event) => {
+    event.stopPropagation(); // Evita que el clic dentro del formulario se propague al contenedor del modal
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
   if (!visible) return null;
 
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-30 backdrop-blur-sm">
-      <div className="bg-white p-2 rounded-md">
-        <p>Mi modal</p>
-        <button
-          onClick={onClose}
-          className="bg-cyan-500 rounded-md px-4 h-12 text-white hover:scale-95 transition text-xl"
-        >
-          Cerrar o actualizar o lo que sea
-        </button>
+    <div
+      className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-30 backdrop-blur-sm"
+      onClick={handleClickOutside}
+    >
+      <div ref={modalRef} className="bg-white p-2 rounded-md">
+        <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center">
+          <div className="bg-white p-2 rounded w-72" onClick={handleFormClick}>
+            <h1 className="font-semibold text-center text-xl text-gray-700">
+              Welcome back
+            </h1>
+            <p className="text-center text-gray-700 mb-5">Sign in</p>
+
+            <div className="flex flex-col">
+  <input
+    type="text"
+    name="lastName"
+    value={formData.lastName}
+    onChange={handleChange}
+    className="border border-gray-700 p-2 rounded mb-5"
+    placeholder="Last Name"
+  />
+  <input
+    type="text"
+    name="birthdate"
+    value={formData.birthdate}
+    onChange={handleChange}
+    className="border border-gray-700 p-2 rounded mb-5"
+    placeholder="Birthdate"
+  />
+  <input
+    type="text"
+    name="documentNumber"
+    value={formData.documentNumber}
+    onChange={handleChange}
+    className="border border-gray-700 p-2 rounded mb-5"
+    placeholder="Document Number"
+  />
+  <input
+    type="text"
+    name="email"
+    value={formData.email}
+    onChange={handleChange}
+    className="border border-gray-700 p-2 rounded mb-5"
+    placeholder="Email"
+  />
+  <input
+    type="text"
+    name="phone"
+    value={formData.phone}
+    onChange={handleChange}
+    className="border border-gray-700 p-2 rounded mb-5"
+    placeholder="Phone"
+  />
+  <input
+    type="text"
+    name="statusDB"
+    value={formData.statusDB}
+    onChange={handleChange}
+    className="border border-gray-700 p-2 rounded mb-5"
+    placeholder="Status DB"
+  />
+</div>
+
+            <div className="text-center">
+              <button className="px-5 py-2 bg-gray-700 text-white rounded">
+                Sign in
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
