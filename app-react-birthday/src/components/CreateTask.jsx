563b1d29ae4b26c939365f476c745153907e1cf7
Pablo Vallejos <faktory.fm@live.com>
2024-02-25 15:44:36 -0300

    tengo el boton de cargar persona

--- app-react-birthday/src/components/CreateTask.jsx
+++ app-react-birthday/src/components/CreateTask.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { toast } from "react-hot-toast";
 import { v4 as uuidv4 } from "uuid";
+import { Link } from "react-router-dom";
+import ModalForm from "./ModalForm";
 
 const CreateTask = ({ tasks, setTasks }) => {
+  const [showMyModal, setShowMyModal] = useState(false);
   const [task, setTask] = useState({
     id: "",
     name: "",
@@ -15,37 +18,45 @@ const CreateTask = ({ tasks, setTasks }) => {
       return toast.error("a task must have more than 3 characters");
 
     if (task.name.length > 100)
-      return toast.error("A task must no bi more than 100 characters");
+      return toast.error("A task must not be more than 100 characters");
 
     setTasks((prev) => {
       const list = prev ? [...prev, task] : [task]; // Use conditional spread
       localStorage.setItem("tasks", JSON.stringify(list));
       return list;
     });
-    toast.success("Tast Created")
+    toast.success("Task Created");
     setTask({
-        id: "",
-        name: "",
-        status: "todo"
+      id: "",
+      name: "",
+      status: "todo",
     });
   };
 
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (showMyModal && !e.target.closest(".modal")) {
+        setShowMyModal(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [showMyModal]);
+
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="border-2 border-slate-400 bg-slate-100 rounded-md mr-4 h-12 w-64 px-1"
-        value={task.name}
-        onChange={(e) =>
-          setTask({ ...task, id: uuidv4(), name: e.target.value })
-        }
-      />
-      <button className="bg-cyan-500 rounded-md px-4 h-12 text-white">
-        Create
+      <ModalForm onClose={() => setShowMyModal(false)} visible={showMyModal} />
+      <button
+        className="bg-cyan-500 rounded-md px-4 h-12 text-white hover:scale-95 transition text-xl"
+        onClick={() => setShowMyModal(true)}
+      >
+        CARGA PERSONA
       </button>
     </form>
   );
 };
-export default CreateTask;
 
-// este el componente donde se crean las tareas y esta en el centro de la pantalla
+export default CreateTask;
