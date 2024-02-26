9f3615c0a5f9bafe5da03b4b3c3b0dab065c6820
Pablo Vallejos <faktory.fm@live.com>
2024-02-26 01:37:10 -0300

    guardar que finci

--- app-react-birthday/src/components/Task.jsx
+++ app-react-birthday/src/components/Task.jsx
@@ -4,26 +4,14 @@ import toast from "react-hot-toast";
 import DeleteSvg from "./svg/DeleteSvg";
 import SeeSvg from "./svg/SeeSvg";
 import UserData from "./UserData";
-import { deleteTask, getTask } from "../api/task.api";
+import { deleteTask } from "../api/task.api";
 
-const Task = ({ task, setTasks }) => {
+const Task = ({ task, tasks, setTasks }) => {
   const [showUserData, setShowUserData] = useState(false);
-  const [userData, setUserData] = useState(null);
 
   const handleOutsideClick = (e) => {
     if (showUserData && !e.target.closest(".modal")) {
       setShowUserData(false);
-    }
-  };
-
-  const handleSeeUserData = async () => {
-    try {
-      const response = await getTask(task.id);
-      setUserData(response.data);
-      setShowUserData(true);
-    } catch (error) {
-      console.error("Error fetching user data:", error);
-      toast.error("Error fetching user data");
     }
   };
 
@@ -37,8 +25,15 @@ const Task = ({ task, setTasks }) => {
 
   const handleDelete = async (id) => {
     try {
+      // Llama a la función para eliminar la tarea de la base de datos
       await deleteTask(id);
-      // handle delete logic...
+      
+      // Actualiza el estado de las tareas en el componente padre
+      const fTasks = tasks.filter((t) => t.id !== id);
+      localStorage.setItem("tasks", JSON.stringify(fTasks));
+      setTasks(fTasks);
+
+      toast("Task Deleted", { icon: "💀" });
     } catch (error) {
       console.error("Error deleting task:", error);
       toast.error("Error deleting task");
@@ -56,7 +51,7 @@ const Task = ({ task, setTasks }) => {
       <div>
         <button
           className="absolute bottom-1 right-1 text-slate-500"
-          onClick={() => handleDelete(task.id)}
+          onClick={() => handleDelete(task.id)} // Aquí se pasa task.id como argumento
         >
           <DeleteSvg />
         </button>
@@ -64,15 +59,14 @@ const Task = ({ task, setTasks }) => {
 
       <div onClick={handleOutsideClick}>
         <button
-          onClick={handleSeeUserData}
+          onClick={() => setShowUserData(true)}
           className="absolute top-1 right-1 flex space-x-2"
         >
           <SeeSvg />
         </button>
         <UserData
-          onClose={() => setShowUserData(false)}
+          onClose={() => setShowUserData(true)}
           visible={showUserData}
-          userData={userData}
         />
       </div>
     </div>
