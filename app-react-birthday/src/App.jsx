70cb60dd7c5c21e9e8f3d01758611966115939bf
Pablo Vallejos <faktory.fm@live.com>
2024-02-25 21:23:01 -0300

    todo funcionando si coneccion a la base de datos

--- app-react-birthday/src/App.jsx
+++ app-react-birthday/src/App.jsx
@@ -1,6 +1,7 @@
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import CreateTask from './components/CreateTask';
+import SaveTask from './components/SaveTask';
 import  ListTasks  from "./components/ListTasks";
 import { useEffect, useState } from "react";
 import { Toaster } from 'react-hot-toast';
@@ -18,13 +19,18 @@ const App=()=> {
   }, []);
   
   return (
-   <DndProvider backend={HTML5Backend}>
-    <Toaster/>
+    <DndProvider backend={HTML5Backend}>
+    <Toaster />
     <div className="bg-slate-200 w-screen h-screen flex flex-col items-center p-3 gap-16 pt-32 ">
-      <CreateTask tasks={tasks} setTasks={setTasks} />
-      <ListTasks tasks={tasks} setTasks={setTasks} />
+      <div className="flex space-x-10">
+        <CreateTask tasks={tasks} setTasks={setTasks} />
+        <SaveTask tasks={tasks} setTasks={setTasks} />
+      </div>
+      <div className="mt-4"> {/* Agrega margen superior para separar las listas */}
+        <ListTasks tasks={tasks} setTasks={setTasks} />
+      </div>
     </div>
-   </DndProvider>
+  </DndProvider>
   );
 }
 export default App
